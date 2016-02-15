38a3f36f1cc4408c8eb4dcc4e04bfd87c887074f
黄伟程 <hwc@putao.cn>
2016-02-15 14:26:14 +0800

    Initial Commit

---
+++ ReactiveCocoaDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReactiveCocoaDemo
+//
+//  Created by hwc on 16/2/15.
+//  Copyright © 2016年 hwc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
